66ef69914a729ab8df22b25af0828f9798162daa
nusaifpk <nusaifputhiyakath1@gmail.com>
2024-12-17 11:28:12 +0530

    update

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,26 @@
-'use client'
+'use client';
 
-import '../styles/Header.css'
-import React, { useState } from 'react'
-import Logo from '../assets/Logo.png'
-import Image from 'next/image'
-import Link from 'next/link'
-import { CiHeart, CiSearch, CiMenuBurger } from 'react-icons/ci'
-import { IoBagHandleOutline } from 'react-icons/io5'
-import { IoIosArrowDown } from 'react-icons/io'
-import { IoClose } from 'react-icons/io5'
+import '../styles/Header.css';
+import React, { useState } from 'react';
+import Logo from '../assets/Logo.png';
+import Image from 'next/image';
+import Link from 'next/link';
+import { CiHeart, CiSearch, CiMenuBurger } from 'react-icons/ci';
+import { IoBagHandleOutline } from 'react-icons/io5';
+import { IoIosArrowDown } from 'react-icons/io';
+import { IoClose } from 'react-icons/io5';
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false); // State for search input
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
 
   return (
     <header className="header-container">
@@ -32,7 +37,18 @@ const Header = () => {
         </div>
 
         <div className="icons-div">
-          <CiSearch className="header-icons" />
+          {/* Search Icon */}
+          <div className="search-container">
+            <CiSearch className="header-icons" onClick={toggleSearch} style={{ cursor: 'pointer' }} />
+            {isSearchOpen && (
+              <input
+                type="search"
+                placeholder="Search..."
+                className="search-input"
+              />
+            )}
+          </div>
+          {/* Other Icons */}
           <CiHeart className="header-icons" />
           <IoBagHandleOutline className="header-icons" />
           <span>
@@ -41,7 +57,7 @@ const Header = () => {
         </div>
       </nav>
 
-{/* MobileSidebar View */}
+      {/* MobileSidebar View */}
       <div className={`mobile-sidebar ${isMenuOpen ? 'open' : ''}`}>
         <div className="sidebar-header">
           <h2>Menu</h2>
@@ -56,6 +72,7 @@ const Header = () => {
         </ul>
       </div>
 
+      {/* Desktop Navigation */}
       <div className="options-nav">
         <ul>
           <li>SHOP</li>
@@ -66,7 +83,7 @@ const Header = () => {
         </ul>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
